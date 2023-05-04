19db33e41b11da2c14415f3abb1fb4a209d009f7
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-04 16:25:22 +0200

    updated filehandler

--- include/filehandler.h
+++ include/filehandler.h
@@ -3,6 +3,7 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
+#include <iostream>
 #include <cstdlib>
 #include <string>
 #include <sys/stat.h>
@@ -12,15 +13,20 @@
 class FileHandler{
 private:
     std::string appDataDir;
+    const std::string appDataName = "data.txt";
     std::string fileName;
 public:
     const static std::string extension;
 private:
     void getAppDataDir(); // Get the path to the directory where the application can store data
     void createAppDataDir(); // Create the application data directory if it doesn't exist
+    void createAppDataFile();   //creates the app data file if it does not exist
+    bool setAppSetting(std::string setting_name, std::string setting_value) const noexcept;
+    std::filesystem::path getAppDataFilePath() const noexcept;
+    void resetAppData() const noexcept;
 public:
     FileHandler();
-    bool setFilePath(std::string path);
+    bool setFilePath(std::string path) noexcept;
     std::string getFilePath() const noexcept;
 };
 
--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -47,14 +47,58 @@ void FileHandler::createAppDataDir(){
     }
 }
 
-FileHandler::FileHandler(){
-    this->createAppDataDir();
+void FileHandler::createAppDataFile(){
+    std::filesystem::path appdatapath = this->getAppDataFilePath();
+    if(!std::filesystem::exists(appdatapath)){
+        std::ofstream(appdatapath.c_str());
+    }
 }
 
-bool FileHandler::setFilePath(std::string path){
-    std::filesystem::path fp{path};
+std::filesystem::path FileHandler::getAppDataFilePath() const noexcept{
+    return std::filesystem::path(this->appDataDir) / std::filesystem::path(this->appDataName);
+}
+
+FileHandler::FileHandler(){
+    this->createAppDataDir();
+    this->createAppDataFile();
+
+}
+
+bool FileHandler::setAppSetting(std::string setting_name, std::string setting_value) const noexcept{
+    /*
+    APP SETTINGS
+    filePath -> Path to the current encryption file
     
-    return std::filesystem::exists(fp);
+    */
+    std::fstream file(this->getAppDataFilePath().c_str());
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::string setting, value;
+        if (!(iss >> setting >> value)){
+            std::cout << "The AppDataFile is not in the right format" << std::endl; // error occured, not in right format
+            std::cout << "Do you wanna reset the appData (y/n): ";
+            std::string tmp;
+            std::cin >> tmp;
+            if(tmp == "y"){
+                this->resetAppData();
+            }
+            return false;
+        } 
+        //WORK
+
+    }
+    return true;
+}
+
+bool FileHandler::setFilePath(std::string path) noexcept{
+    std::filesystem::path fp{path};
+    bool exist = std::filesystem::exists(fp);
+    if(exist){
+        this->setAppSetting("filePath", path);
+    }
+    return exist;
 }
 
 std::string FileHandler::getFilePath() const noexcept{
