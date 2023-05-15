c13c621be87ac70c8f4faa436f9e18fe4803b8ce
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-15 19:54:09 +0200

    added the password data class

--- include/file_data.h
+++ include/file_data.h
@@ -5,7 +5,11 @@
 #include "bytes.h"
 
 class FileData{
+private:
+    virtual bool isDataValid(Bytes bytes) noexcept = 0;
+public:
     virtual Bytes run(Bytes) = 0;
+    virtual std::string getError() = 0;
 };
 
 #endif //FILEDATA_H
---
+++ include/password_data.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef PWDDATA_H
+#define PWDDATA_H
+
+#include <unordered_map>
+#include "file_data.h"
+
+
+class PasswordData : public FileData{
+private:
+    std::unordered_map<std::string, std::vector<std::string>> siteMap;
+    std::string error;
+    bool getData(Bytes bytes) noexcept;
+    void printHelp() const noexcept;
+    Bytes getBytes() const;
+    void showSets(std::string substring) const noexcept;
+    void addPw(std::string input) const noexcept;
+    void remPw(std::string input) const noexcept;
+    void editPw(std::string input) const noexcept;
+public:
+    Bytes run(Bytes bytes);
+    std::string getError() const noexcept;
+};
+
+#endif //PWDDATA_H
