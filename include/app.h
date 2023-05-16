d374402fbd46818f5e51db953fb402c55441f51b
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-16 22:43:06 +0200

    added comments

--- include/app.h
+++ include/app.h
@@ -1,3 +1,7 @@
+/*
+this module holds the application class which is the interface between the main function and the rest of the programm
+the run() method is the encapsulated main method
+*/
 #pragma once
 #ifndef APP_H
 #define APP_H
@@ -7,17 +11,18 @@
 
 class App{
 private:
-    std::string filePath;
-    FileHandler FH;
+    std::string filePath;   //stores the path to the encryption file
+    FileHandler FH;         //stores the FileHandler object
 
 private:
+    //this method is printing some start informations. If no filepath is set we create a new file in the current dir
     void printStart();
-    std::string askForPasswd() const noexcept;
-    unsigned char askForFileMode() const noexcept;
-    unsigned char askForHashMode() const noexcept;
-    unsigned char askForChainHashMode() const noexcept;
-    unsigned long askForIters(std::string msg) const noexcept;
-    Bytes askForHeader() const;
+    std::string askForPasswd() const noexcept;      //asks the user for a password and validates the input format
+    unsigned char askForFileMode() const noexcept;  //asks the user for a file mode and validates the input format
+    unsigned char askForHashMode() const noexcept;  //asks the user for a hash mode and validates the input format
+    unsigned char askForChainHashMode() const noexcept;         //asks the user for a chain hash mode and validates the input format
+    unsigned long askForIters(std::string msg) const noexcept;  //asks the user for an iteration number and validates the input format
+    Bytes askForHeader() const;     //asks the user for data that is put into a header for the file
 public:
     App();
     bool run();
