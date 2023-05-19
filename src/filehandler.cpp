4a9cda8fef9e903dcaa19e97f54f7c590b4c8a0e
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-19 21:36:42 +0200

    comment the File Handler class

--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -1,32 +1,43 @@
+/*
+this file contains the implementations of the FileHandler class
+*/
 #include "filehandler.h"
 
-const std::string FileHandler::extension = ".enc";
+//set static extension var
+const std::string FileHandler::extension = ".enc";  //encryption files (.enc)
 
 FileHandler::FileHandler(){
+    //constructs a file handler object by creating the app data file if it does not exist
     this->createAppDataDir();
     if(!this->isAppDataFile()){
         this->createAppDataFile();
     }
+    //filepath to the enc file is empty at default
     this->encryption_filepath = "";
+    //get the app data setting for the enc file path
     std::optional<std::string> encryption_filepath = getAppSetting("filePath");
     if(encryption_filepath.has_value()){
+        //file path is set
         if(!std::filesystem::exists(encryption_filepath.value())){
+            //but it does not exist on the pc, remove this setting
             this->removeAppSetting("filePath");
         }else{
+            //and it exists. Set this path
             this->encryption_filepath = encryption_filepath.value();
         }
     }
-
+    //if there was no valid enc file path found, it is empty
 }
 
 void FileHandler::getAppDataDir(){
+    //gets the app data directory depending on the os
 #if defined(_WIN32)
     // Get the user's app data directory on Windows
     char* appDataDirChar = nullptr;
     size_t len = 0;
     _dupenv_s(&appDataDirChar, &len, "APPDATA");
     if (appDataDirChar != nullptr) {
-        this->appDataDir = appDataDirChar;
+        this->appDataDir = appDataDirChar; //WORK, should be tested if an extended path has to be added
         free(appDataDirChar);
     }
 #elif defined(__APPLE__)
@@ -52,9 +63,10 @@ void FileHandler::getAppDataDir(){
 }
 
 void FileHandler::createAppDataDir(){
-    this->getAppDataDir();
+    //creates the app data directory if it does not exist
+    this->getAppDataDir();  //gets the directory
     std::string appDataDir = this->appDataDir;
-    if (!appDataDir.empty()) {
+    if (!appDataDir.empty()) {  //not yet created?
         struct stat info;
         if (stat(appDataDir.c_str(), &info) != 0 || !(info.st_mode & S_IFDIR)) {
             // The directory doesn't exist, so create it
@@ -64,13 +76,15 @@ void FileHandler::createAppDataDir(){
 }
 
 void FileHandler::createAppDataFile(){
-    std::filesystem::path appdatapath = this->getAppDataFilePath();
+    //creates the app data file
+    std::filesystem::path appdatapath = this->getAppDataFilePath(); //gets the file path
     if(!std::filesystem::exists(appdatapath)){
-        std::ofstream(appdatapath.c_str());
+        std::ofstream(appdatapath.c_str());     //create the file if it does not exist
     }
 }
 
 std::filesystem::path FileHandler::getAppDataFilePath() const noexcept{
+    //gets the file path to the app data file by concatinating the dir path with the file name
     return std::filesystem::path(this->appDataDir) / std::filesystem::path(this->appDataName);
 }
 
@@ -79,24 +93,29 @@ void FileHandler::resetAppData() const noexcept{
 }
 
 bool FileHandler::removeAppSetting(const std::string setting_name) const{
+    //remove a given setting from the app data
     if(!this->isAppDataFile()){
+        //could not found the app data file
         throw std::runtime_error("App data file not found");
     }
-    std::fstream file(this->getAppDataFilePath().c_str());
+    std::fstream file(this->getAppDataFilePath().c_str());  //create the file stream
     std::stringstream file_content;     //stores the data of the file
     std::string line;
     while (std::getline(file, line)){
+        //reads the file line by line
         std::istringstream iss(line);
         std::string setting, value;
         if (!(iss >> setting >> value)){
+            //if the line does not conatin two strings (setting, value pair)
             std::cout << "The AppDataFile is not in the right format" << std::endl; // error occured, not in right format
             std::cout << "Do you wanna reset the appData (y/n): ";
             std::string tmp;
             std::cin >> tmp;
             if(tmp == "y"){
+                //reset the app data file to get the right format
                 this->resetAppData();
             }
-            return false;
+            return false;   //cannot proceed due to incorect file format
         } 
         std::cout << setting << "|" << value << std::endl;  //DEBUGONLY
         if(!(setting == setting_name)){
@@ -104,30 +123,36 @@ bool FileHandler::removeAppSetting(const std::string setting_name) const{
             file_content << line << std::endl;
         }
     }
-    std::ofstream write_file;
+    std::ofstream write_file;   
+    //opens the file again in write mode
     write_file.open(this->getAppDataFilePath().c_str(), std::ofstream::out | std::ofstream::trunc);
-    write_file << file_content.str();       //writes the file again with the read content without the deleted setting
+    write_file << file_content.str();       //writes the file with the read content without the deleted setting
     return true;
 }
 
 std::optional<std::string> FileHandler::getAppSetting(const std::string setting_name) const{
-    if(!this->isAppDataFile()){
+    //gets the value for a given setting from the app data file
+    if(!this->isAppDataFile()){ 
+        //could not found the app data file
         throw std::runtime_error("App data file not found");
     }
-    std::fstream file(this->getAppDataFilePath().c_str());
+    std::fstream file(this->getAppDataFilePath().c_str());  //create the file stream
     std::string line;
     while (std::getline(file, line)){
+        //reads the file line by line
         std::istringstream iss(line);
         std::string setting, value;
         if (!(iss >> setting >> value)){
+            //if the line does not conatin two strings (setting, value pair)
             std::cout << "The AppDataFile is not in the right format" << std::endl; // error occured, not in right format
             std::cout << "Do you wanna reset the appData (y/n): ";
             std::string tmp;
             std::cin >> tmp;
             if(tmp == "y"){
+                //reset the app data file to get the right format
                 this->resetAppData();
             }
-            return{};
+            return{};   //cannot proceed due to incorect file format
         } 
         std::cout << setting << "|" << value << std::endl;  //DEBUGONLY
         if(setting == setting_name){
@@ -135,89 +160,101 @@ std::optional<std::string> FileHandler::getAppSetting(const std::string setting_
             return value;
         }
     }
-    return{};
+    return{};   //setting is not set in the appdata file, return empty optional
 }
 
 bool FileHandler::isAppDataFile() const noexcept{
+    //checks if the app data file exists on the system
     return std::filesystem::exists(this->getAppDataFilePath().c_str());
 }
 
 bool FileHandler::setAppSetting(const std::string setting_name, const std::string setting_value) const{
     /*
+    sets a new app setting with the provided name and value
     APP SETTINGS
-    filePath -> Path to the current encryption file
+    for docs look into docs/appdata.md
     */
     if(!this->isAppDataFile()){
+        //could not found the app data file
         throw std::runtime_error("App data file not found");
     }
-    std::fstream file(this->getAppDataFilePath().c_str());
+    std::fstream file(this->getAppDataFilePath().c_str());  //create the file stream
     std::stringstream file_content;     //stores the data of the file
     std::string line;
-    int line_number = -1;
     while (std::getline(file, line)){
-        line_number++;
+        //reads the app data file line by line
         std::istringstream iss(line);
         std::string setting, value;
         if (!(iss >> setting >> value)){
+            //if the line does not conatin two strings (setting, value pair)
             std::cout << "The AppDataFile is not in the right format" << std::endl; // error occured, not in right format
             std::cout << "Do you wanna reset the appData (y/n): ";
             std::string tmp;
             std::cin >> tmp;
             if(tmp == "y"){
+                //reset the app data file to get the right format
                 this->resetAppData();
             }
-            return false;
+            return false;   //cannot proceed due to incorect file format
         } 
         std::cout << setting << "|" << value << std::endl;  //DEBUGONLY
         if(!(setting == setting_name)){
-            //setting was not set previously, we have to save it, delete if it is the setting we wanna change
+            //setting was not set previously, we have to save it, drop if it is the setting we wanna change
             file_content << line << std::endl;
         }
     }
     //add the new setting to the file content stream
     file_content << setting_name << " " << setting_value << std::endl;
 
-    std::ofstream write_file;
+    std::ofstream write_file;   
+    //open the file again in write mode
     write_file.open(this->getAppDataFilePath().c_str(), std::ofstream::out | std::ofstream::trunc);
     write_file << file_content.str();       //writes the file again with the read content and the added setting
     return true;
 }
 
 bool FileHandler::setEncryptionFilePath(const std::string path) noexcept{
+    //set a new encryption file path
     std::filesystem::path fp{path};
-    bool exist = std::filesystem::exists(fp);
+    bool exist = std::filesystem::exists(fp);   //checks whether the provided path exists
     if(exist){
+        //if it exists, change the path in the app data 
         this->setAppSetting("filePath", path);
         this->encryption_filepath = path;
     }
-    return exist;
+    return exist;   //return the success bool
 }
 
 std::string FileHandler::getEncryptionFilePath() const noexcept{
+    //getter for the encryption filepath
     return this->encryption_filepath;
 }
 
 Bytes FileHandler::getFirstBytes(const int num) const{
+    //reads the num first bytes of the encryption file
     if(this->encryption_filepath.empty()){
+        //no encryption file path set
         throw std::runtime_error("Encrypted filepath is empty");
     }
-    std::ifstream file(this->encryption_filepath.c_str()); 
+    std::ifstream file(this->encryption_filepath.c_str());  //create the file stream 
     if(!file){
+        //the set encryption file does not exist on the system
         throw std::runtime_error("Encrypted file not found in filepath");
     }
     // get length of file:
-    file.seekg (0, file.end);
-    int length = file.tellg();
-    file.seekg (0, file.beg);
+    file.seekg (0, file.end);   //jump to the end
+    int length = file.tellg();  //saves the position
+    file.seekg (0, file.beg);   //jumps to the start again
     if(length < num){
-        //not enough characters to read
+        //not enough characters to read (file is not long enough)
         throw std::length_error("File contains to few characters");
     }
-    char buf[num];
-    file.read(buf, num);
+    char buf[num];  //creates a buffer to hold the read bytes
+    file.read(buf, num);    //reads into the buffer
+    //transform the char array into an Bytes object 
     Bytes ret = Bytes();
     for(char c : buf){
-        ret.addByte(static_cast<unsigned char>(c));
+        ret.addByte(static_cast<unsigned char>(c)); //cast from char to unsigned char
     }
     return ret;
 }
