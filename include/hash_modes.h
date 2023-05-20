1ff9bccc30c62436dbf5827d99db81ac9fa3e002
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-20 21:58:54 +0200

    comment hash modes class

--- include/hash_modes.h
+++ include/hash_modes.h
@@ -10,9 +10,17 @@
 #include "sha512.h"
 
 class HashModes{
+    /*
+    static class for the hash modes.
+    You can get infos about a hash mode or check if it is valid
+    The important thing is that you can get the corresponding Hash class for that hash mode
+    */
 public:
+    //checks if the hash mode exists (has to be in a defined range)
     static bool isModeValid(const unsigned char hash_mode) noexcept;
+    //gets the hash sub class (i.e sha256) to perform a hash (strategy pattern)
     static Hash* getHash(const unsigned char hash_mode);
+    //gets some information about that hash mode (info about the hash function)
     static std::string getInfo(const unsigned char hash_mode) noexcept;
 };
 
--- src/hash_modes.cpp
+++ src/hash_modes.cpp
@@ -1,3 +1,6 @@
+/*
+this file contains implementations of HashModes class
+*/
 #include "hash_modes.h"
 
 bool HashModes::isModeValid(const unsigned char hash_mode) noexcept{
@@ -6,6 +9,7 @@ bool HashModes::isModeValid(const unsigned char hash_mode) noexcept{
 }
 
 Hash* HashModes::getHash(const unsigned char hash_mode){
+    //gets the concrete hash sub class
     switch(hash_mode){
         case 1: //sha256
             return new sha256();
@@ -13,14 +17,17 @@ Hash* HashModes::getHash(const unsigned char hash_mode){
             return new sha384();
         case 3: //sha512
             return new sha512();
-        default:
+        default:    //hash mode is out of range
             throw std::invalid_argument("hash mode does not exist");
     }
 }
 
 std::string HashModes::getInfo(const unsigned char hash_mode) noexcept{
+    //gets some information about this hash mode (hash function)
     std::stringstream msg{}; 
+    //start with a base string
     msg << "The hash mode " << +hash_mode << " corresponds to hash function: ";
+    //add the information based on the mode
     switch (hash_mode){
         case 1: //sha256
             msg << "Sha256";
