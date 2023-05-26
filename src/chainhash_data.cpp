23a2bec2470d1b4dcc45ecc061bd5cb63fcc56a5
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-05-26 21:24:58 +0200

    adapted the chainhash data to format + docs

--- src/chainhash_data.cpp
+++ src/chainhash_data.cpp
@@ -11,71 +11,11 @@ void ChainHashData::clear() noexcept{
     this->name_lens.clear();
 }
 
-ChainHashData::ChainHashData(std::string format){
-    //format infos in chainhash_modes.md
-    //format <length in bytes>B <name>
+ChainHashData::ChainHashData(Format format): 
+    format(format){
     this->clear();  //clears the data from the object
-    this->format = format;
-    if(format.empty()){
-        //got an empty format, user wants an empty datablock
-        return;
-    }
-    //saves all data names, to make sure that there are no duplicates
-    std::vector<std::string> data_names;
-    size_t ind = 0;
-    //data_name is true if the part that is looked on is a data_name.
-    //if it is false, that means we are looking on the size part
-    bool data_name = false;
-    //stores the length of the next data (because we get the length before the name)
-    unsigned char data_len = 0;
-    //loops over every format substring seperated by " "
-    while ((ind = format.find(" ")) != std::string::npos) {
-        std::string substring = format.substr(0, ind);  //gets the substring with the given index
-        if(substring.empty()){
-            throw std::invalid_argument("format is not valid. Got some empty substring");
-        }
-        if(!data_name){
-            //getting the size of the part
-            if(!endsWith(substring, "B")){
-                //size does not end with B (wrong format)
-                throw std::invalid_argument("format is not valid. Contains no B on the end of size part.");
-            }
-            substring.erase(substring.end());   //delete the B
-            int size_int = 0;
-            //if the format is *B, that means that any length (greater than zero is accepted)
-            //we mark this with a length of zero
-            if(substring != "*"){
-                try{
-                    size_int = std::stoi(substring);    //transform the string to an int
-                }catch(std::exception){
-                    //could not convert string to int
-                    throw std::invalid_argument("format is not valid. Size could not be converted to int");
-                }
-                if(!(0 < size_int < 256)){
-                    //provided size is not valid
-                    throw std::invalid_argument("format is not valid. Size has to be between 0 and 256 exclusive.");
-                }
-            }
-            //valid size
-            data_len = size_int;
-        }else{
-            if(std::find(data_names.begin(), data_names.end(), substring) != data_names.end()){
-                //found the current data name already in the list
-                throw std::invalid_argument("format is not valid. Found multiple parts with the same name");
-            }
-            data_names.push_back(substring);
-            this->name_lens.push_back(NameLen(substring, data_len));
-        }
-        format.erase(0, ind + 1);    //erase the substring + the separator " ", which's length is 1
-        data_name = !data_name;     //flip the bool, data names and sizes are alternating
-    }
-    if(!(0 < this->name_lens.size() < 256)){
-        //the algorithm did not add anything to the list, but the user entered a non empty format string
-        //or the user inputs more than 256 datasets
-        std::cout << "Format has to contain between 0 and 256 entries exclusive";
-        std::cout << "or the format string has to be empty for 0 entries" << std::endl;
-        throw std::invalid_argument("format is not valid. Got invalid entry number from a non empty format");
-    }
+    //gets the name len data to name the data parts and know which length they are
+    this->name_lens = format.getNameLenList();
 }
 
 bool ChainHashData::isComplete() const noexcept{
@@ -85,7 +25,7 @@ bool ChainHashData::isComplete() const noexcept{
     return (this->data_parts.size() == this->name_lens.size());
 }
 
-bool ChainHashData::isCompletedFormat(std::string format) const noexcept{
+bool ChainHashData::isCompletedFormat(Format format) const noexcept{
     //checks if a given format is set
     return (format == this->format && this->isComplete());
 }
