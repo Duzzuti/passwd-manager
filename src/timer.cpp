02af3969c6ddcbc66048b9e6749ec6323bd916a0
0xedl <122504006+0xedl@users.noreply.github.com>
2023-06-01 12:18:39 -0400

    created timer source class
    a start() method to start the timer
    a stop() method to stop the timer and saving the time needed since the last start() or recordTime() call
    a recordTime() method to save the time needed since the last start() or recordTime() call
    a peekTime() method which returns the time passed since the last record or start call
    a getAverageTime() method which returns the average time per record and stop (getTime() / getLaps())
    a getTime() method which returns the timedelta between the start and stop call
    a getLaps() method which returns the number of recordTime() and stop() calls (the total number of measurements you made)

---
+++ src/timer.cpp
@@ -0,0 +1,55 @@
+#include "timer.h"
+
+void timer::start(){
+    if (started) {
+        throw std::runtime_error("Timer has already been started");
+    }
+    started = true;
+    stopped = false;
+    last_time = std::chrono::steady_clock::now();
+}
+
+void timer::stop(){
+        this->require_started();
+        this->stopped = true;
+        this->duration = std::chrono::steady_clock::now() - this->start_time;
+}
+
+unsigned long timer::recordTime(){
+        require_started();
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time);
+        laps.push_back(elapsed.count());
+        last_time = now;
+}
+
+unsigned long timer::peekTime(){
+        require_started();
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time);
+        return elapsed.count();
+}
+
+unsigned long timer::getAverageTime() {
+        require_stopped();
+        if (laps.empty()) {
+        return 0;
+        } else {
+        unsigned long sum = 0;
+        for (auto lap : laps) {
+            sum += lap;
+        }
+        return sum / laps.size();
+        }
+}
+
+unsigned long timer::getTime(){
+        require_stopped();
+        return getAverageTime() * laps.size();
+}
+
+unsigned long timer::getLaps(){
+        require_stopped();
+        return laps.size();
+}
+
