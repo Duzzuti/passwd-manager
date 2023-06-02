2e8cade4bb1066c1b4aa56196016467bb957082b
Duz <moritz.richter1@mailbox.tu-dresden.de>
2023-06-02 16:06:48 +0000

    add stringToBytes utility function

--- include/utility.h
+++ include/utility.h
@@ -9,12 +9,16 @@ if a functionality is used in different files or scenarios, it should be defined
 #include <iostream>
 #include <vector>
 
+#include "bytes.h"
+
 // checks if a string ends with a string
 bool endsWith(const std::string& fullString, const std::string& ending) noexcept;
 // transforms a u_int64_t into a byte array
 std::vector<unsigned char> LongToCharVec(const u_int64_t a) noexcept;
 // transforms a byte array into a string
 std::string charVecToString(const std::vector<unsigned char> v) noexcept;
+// transforms a string into a byts object
+Bytes stringToBytes(const std::string str) noexcept;
 // gets the byte length of a long number
 unsigned int getLongLen(const u_int64_t num) noexcept;
 
