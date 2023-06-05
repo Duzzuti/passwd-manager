cc9e239a86a265b5fba5650976ce7f2e41042fc3
Marc Gavilan <marcgavilangil@gmail.com>
2023-06-05 14:12:50 +0000

    feat: Prepare CLI frontend with ftxui: https://github.com/ArthurSonzogni/FTXUI

---
+++ src/cli.cpp
@@ -0,0 +1,16 @@
+#include "cli.h"
+
+#include "ftxui/component/button.hpp"
+#include "ftxui/component/checkbox.hpp"
+#include "ftxui/component/container.hpp"
+#include "ftxui/component/input.hpp"
+#include "ftxui/component/menu.hpp"
+#include "ftxui/component/radiobox.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/toggle.hpp"
+#include "ftxui/dom/elements.hpp"
+
+/**
+ * Here goes the code for the CLI frontend
+ * 
+*/
