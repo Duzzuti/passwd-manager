36f2acdee8d0c4f9e277e06bb721b2c31cfee84a
Duzzuti <duzzuti@gmail.com>
2023-07-15 14:43:05 +0000

    added hash function for blocks

--- include/hash.h
+++ include/hash.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "bytes.h"
+#include "block.h"
+
 class Hash {
     /*
     abstract hash class
@@ -9,6 +11,7 @@ class Hash {
    public:
     Hash() = default;                                     // it needs a default constructor
     virtual int getHashSize() const noexcept = 0;         // a getter for the byte len of the hash
+    Bytes hash(const Block block) const;                  // a hash function that takes a Block object
     virtual Bytes hash(const Bytes bytes) const = 0;      // a hash function that takes a Bytes object
     virtual Bytes hash(const std::string str) const = 0;  // a second hash function that takes a string
     virtual ~Hash(){};
