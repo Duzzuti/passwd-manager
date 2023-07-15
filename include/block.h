ffa86e07637c780c0f9882f34326131504ecbf4f
Duzzuti <duzzuti@gmail.com>
2023-07-15 19:36:33 +0000

    refactored block class made it abstract, there can be a DecodeBlock and EncodeBlock removed LOTS of functionality that is no longer needed The new blockchain class will take care of the most work the block only need to add a salt to the input data

--- include/block.h
+++ include/block.h
@@ -1,45 +1,27 @@
 #pragma once
 
-#include "blockchainBase.h"
 #include "bytes.h"
 
 class Block {
     /*
-    the block class represents one block of the blockchain
-    it has data, salt and the passwordhash to compute the encoded data
+    the abstract block class represents one block of the blockchain
+    one block has a fixed length and a salt that is used to encrypt the input data
+    if new data is pushed into the block (with addData) it is encrypted/decrypted with the salt
+    that should be implemented in the derived class
+    after encrypting/decrypting the data it is added to the data member
+    if the block is completed (the data member is full) the result can be retrieved with getResult
     */
-    friend BlockChain;  // the blockchain has to access private members (like getData)
    private:
-    int len;             // block len in bytes
-    Bytes data;          // plain text data
-    Bytes salt;          // salt to keep the passwordhash secure even if the attacker knows the data
-    Bytes passwordhash;  // passwordhash (hash that is derived from the password)
-    Bytes encoded;       // the encoded data
+    unsigned char block_len;             // block len in bytes
+    Bytes data;          // data produced by the block
+    Bytes salt;          // salt for decryption. The salt is added/subtracted from the input data
 
-   private:
-    Bytes getSalt() const noexcept;          // getter for the salt bytes
-    Bytes getPasswordHash() const noexcept;  // getter for the passwordhash bytes
-    Bytes getData() const noexcept;          // getter for the data bytes
    public:
-    // encrypt
-    Block();                                                                             // creates a block with a length of zero (you have to call setLen to use this block)
-    Block(const int len, const Bytes data, const Bytes salt, const Bytes passwordhash);  // creates a block with all necessary data to encode
-    void setLen(const int len);                                                          // sets the len of the block (note that this only works if no other data is set yet)
-    void setData(const Bytes data);                                                      // sets the data of the block (note that the length has to be right)
-    void setPasswordHash(const Bytes passwordhash);                                      // sets the passwordhash of the block (note that the length has to be right)
-    void setSalt(const Bytes salt);                                                      // sets the salt of the block (note that the length has to be right)
-    int getLen() const noexcept;                                                         // getter for the block length
-    Bytes getEncoded() const noexcept;                                                   // getter for the encoded bytes
-    bool isReadyForEncode() const noexcept;                                              // returns true if the block has all data to compute the encoded data
-    void calcEncoded();                                                                  // computes the encoded data
-    bool isEncoded() const noexcept;                                                     // returns true if the data has been encoded
-
-    // decrypt
-    Block(const Bytes encoded);              // creates a block with only encoded data (to decrypt you have to set a password hash and a salt)
-    void setEncoded(const Bytes encoded);    // setter for encoded data
-    bool isReadyForDecode() const noexcept;  // returns true if the block has all data to decrypt
-    void calcData();                         // decrypt the encoded data to plain data
-    bool isDecoded() const noexcept;         // returns true if the encoded data was decrypted
-
-    void clear() noexcept;  // clears the block data and sets length to zero
+    // delete the default constructor
+    // blocks should only be created with a valid length and salt
+    Block() = delete;
+    Block(const unsigned char len, const Bytes salt);                                    // creates a block with a length and a salt that is used to encrypt input data
+    unsigned char getFreeSpace() const noexcept;                                         // returns the available space in the block
+    virtual void addData(const Bytes data);                                              // adds new data to the block (this data is encrypted with the salt)
+    Bytes getResult() const;                                                             // getter for the result data (throws if the block is not completed)
 };
--- src/block.cpp
+++ src/block.cpp
@@ -1,133 +1,30 @@
+/*
+contains the implementation of the abstract Block class
+*/
 #include "block.h"
 
-Block::Block() {
-    this->len = 0;
-    this->data = Bytes();
-    this->encoded = Bytes();
-    this->passwordhash = Bytes();
-    this->salt = Bytes();
-}
-
-Block::Block(const int len, const Bytes data, const Bytes salt, const Bytes passwordhash) {
+Block::Block(const unsigned char len, const Bytes salt) {
     if (len <= 0) {
         // invalid block length
         throw std::range_error("length of the block cannot be negative or zero");
     }
-    if (!((salt.getLen() == len) && (passwordhash.getLen() == len) && (len == data.getLen()))) {
-        // block length was not fulfilled by the data
-        throw std::length_error("lengths of bytes dont match with the given length");
-    }
-    this->len = len;
-    this->data = data;
-    this->encoded = Bytes();
-    this->passwordhash = passwordhash;
-    this->salt = salt;
-}
-
-void Block::setLen(const int len) {
-    if (len > 0 && this->data.getLen() == 0 && this->encoded.getLen() == 0 && this->salt.getLen() == 0 && this->passwordhash.getLen() == 0) {
-        this->len = len;
-    } else {
-        // you cannot set the block length because its not a valid length or other data was set before
-        throw std::length_error("there already is some data stored in the block. Clear it first");
-    }
-}
-
-void Block::setData(const Bytes data) {
-    if (data.getLen() != this->getLen() || this->getLen() <= 0) {
-        // you cannot set the data because it does not fullfil the block length
-        throw std::length_error("length of data bytes does not match with the block length");
-    }
-    this->data = data;
-}
-
-void Block::setPasswordHash(const Bytes passwordhash) {
-    if (passwordhash.getLen() != this->getLen() || this->getLen() <= 0) {
-        // you cannot set the passwordhash because it does not fullfil the block length
-        throw std::length_error("length of passwordhash bytes does not match with the block length");
-    }
-    this->passwordhash = passwordhash;
-}
-
-void Block::setSalt(const Bytes salt) {
-    if (salt.getLen() != this->getLen() || this->getLen() <= 0) {
-        // you cannot set the salt because it does not fullfil the block length
+    if (salt.getLen() != len) {
+        // block length was not fulfilled by the salt
         throw std::length_error("length of salt bytes does not match with the block length");
     }
+    this->block_len = len;
+    this->data = Bytes();
     this->salt = salt;
 }
 
-int Block::getLen() const noexcept { return this->len; }
-
-Bytes Block::getEncoded() const noexcept { return this->encoded; }
-
-bool Block::isReadyForEncode() const noexcept {
-    // the block length has to be valid and all data has to be set with the block size length
-    return (this->getLen() > 0 && (this->getLen() == this->data.getLen()) && (this->getLen() == this->passwordhash.getLen()) && (this->getLen() == this->salt.getLen()));
+unsigned char Block::getFreeSpace() const noexcept { 
+    // returns the number of bytes that can be added to the block until it is completed
+    return this->block_len - this->data.getLen();
 }
 
-void Block::calcEncoded() {
-    if (!this->isReadyForEncode()) {
-        // block was not ready for encode
-        throw std::logic_error("Block does not have all needed data (block length, data, salt and passwordhash)");
-    }
-    this->encoded = this->data + this->salt + this->passwordhash;  // bytes operator overload
+Bytes Block::getResult() const {
+    // returns the resulting data of the block if the block is completed
+    if(this->getFreeSpace() != 0)
+        throw std::logic_error("block is not completed");
+    return this->data;
 }
-
-bool Block::isEncoded() const noexcept {
-    // encoded data length is equal to block length and greater zero
-    return (this->getLen() > 0 && this->getLen() == this->encoded.getLen());
-}
-
-Block::Block(const Bytes encoded) {
-    if (encoded.getLen() <= 0) {
-        // provided data length is invalid
-        throw std::range_error("length of the block cannot be negative or zero");
-    }
-    this->encoded = encoded;
-    this->len = encoded.getLen();
-    this->data = Bytes();
-    this->passwordhash = Bytes();
-    this->salt = Bytes();
-}
-
-void Block::setEncoded(const Bytes encoded) {
-    if (this->getLen() != encoded.getLen() || this->getLen() <= 0) {
-        // you cannot set the encoded because it does not fullfil the block length
-        throw std::length_error("length of encoded bytes does not match with the block length");
-    }
-    this->encoded = encoded;
-}
-
-bool Block::isReadyForDecode() const noexcept {
-    // the block length has to be valid and all data has to be set with the block size length
-    return (this->getLen() > 0 && (this->getLen() == this->encoded.getLen()) && (this->getLen() == this->salt.getLen()) && (this->getLen() == this->passwordhash.getLen()));
-}
-
-void Block::calcData() {
-    if (!this->isReadyForDecode()) {
-        // block was not ready for decoding
-        throw std::logic_error("Block does not have all needed data (block length, encoded, salt and passwordhash)");
-    }
-    this->data = this->encoded - this->salt - this->passwordhash;  // bytes operator overload
-}
-
-bool Block::isDecoded() const noexcept {
-    // data has to be set and valid
-    return (this->getLen() > 0 && this->data.getLen() == this->getLen());
-}
-
-void Block::clear() noexcept {
-    // resets the block
-    this->len = 0;
-    this->data = Bytes();
-    this->encoded = Bytes();
-    this->passwordhash = Bytes();
-    this->salt = Bytes();
-}
-
-Bytes Block::getSalt() const noexcept { return this->salt; }
-
-Bytes Block::getPasswordHash() const noexcept { return this->passwordhash; }
-
-Bytes Block::getData() const noexcept { return this->data; }
