02e2a95a8a413dab932fcbcbf8bfe35f74929c87
Duzzuti <duzzuti@gmail.com>
2023-07-15 21:27:07 +0000

    added blockchain docs + blocks are now ptr The blocks in the blockchain have to be pointers because Block is now abstract

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,23 +1,52 @@
 #pragma once
+#include <memory>
 #include "block.h"
 #include "hash.h"
 
 class BlockChain {
+    /*
+    The blockchain class represents a queue (vector) of blocks
+    A BlockChain is initialized with a hash function and a password hash and an encrypted salt
+    The hash function should be got from the DataHeader´s hash mode
+    The password hash is the hash of the correct password
+    The encrypted salt is got from the DataHeader
+
+    The blockchain class is abstract, because you use it differently for encryption and decryption
+
+    You can add new data that should be encrypted/decrypted with addData
+    that will add the data to the last block of the chain and if the block is completed it will add a new block
+    for each block there is a salt generated that is used to encrypt/decrypt the data
+    the salt is generated with the password hash and the encrypted salt as well as the last blocks hash
+    this is done with the SaltIterator class
+    It is:
+        - deterministic
+        - you can only get the next salt if you have the last block hash
+        - the first salt is known to the attacker and even if he knows the data of the first block 
+        he cannot get the next salt or passwordhash (except if he knows the inverse hash function :))
+    */
    private:
     class SaltIterator {
+        /*
+        the SaltIterator class is used to generate the salts for the blocks
+        it is initialized with the password hash and the encrypted salt
+        for every block it generates a new salt with the hash of the last block
+        */
        private:
-        bool ready;
-        bool first;
-        Bytes hash;
-        Bytes salt;
-        const Hash* hashObj;
+        bool ready; // is the iterator ready to generate salts
+        bool first; // is this the first salt/block
+        Bytes hash; // the current hash (first is the passwordhash)
+        Bytes salt; // the current salt (first is the encrypted salt)
+        const Hash* hashObj;    // the hash object that provides the hash function
 
        public:
         SaltIterator() {
+            // not ready yet, has to be initialized first (with init)
             this->ready = false;
             this->first = true;
         }
         void init(const Bytes pwhash, const Bytes enc_salt, const Hash* hashObj) {
+            // note that SaltIterator does not take ownership of the hashObj pointer you need to delete it yourself
+            // initializes the iterator with the password hash and the encrypted salt
             if (hashObj == nullptr) throw std::invalid_argument("hash cannot be nullptr");
             if (pwhash.getLen() != hashObj->getHashSize()) throw std::invalid_argument("passwordhash has to be the same size as the hash");
             if (enc_salt.getLen() != hashObj->getHashSize()) throw std::invalid_argument("enc_salt has to be the same size as the hash");
@@ -27,9 +56,12 @@ class BlockChain {
             this->hashObj = hashObj;
         }
         Bytes next(Bytes last_block_hash = Bytes(0)) {
+            // generates the next salt with the last block hash
             if (!first) {
+                // if this is not the first block, the last_block_hash has to be provided
                 if (last_block_hash.getLen() != this->hashObj->getHashSize()) throw std::invalid_argument("last_block_hash has to be the same size as the hash");
             } else {
+                // if this is the first block, the last_block_hash is set to 0
                 first = false;
                 // generate a Bytes object with the size of the hash with all bytes set to 0
                 last_block_hash = Bytes(0);
@@ -37,30 +69,40 @@ class BlockChain {
             }
             if (last_block_hash.getLen() != this->hashObj->getHashSize()) throw std::invalid_argument("last_block_hash has to be the same size as the hash");
             if (!this->ready) throw std::runtime_error("SaltIterator is not ready, call init first");
+            // generate the next hash and salt by hashing the last hash and salt with the last block hash
             this->hash = this->hashObj->hash(this->hash + this->salt + last_block_hash);
+            // note that the salt is not equal to the hash because the salt is generated with the new hash
             this->salt = this->hashObj->hash(this->hash + this->salt + last_block_hash);
+            // return a hash of the hash and the salt to make sure you cannot calculate any of the other values
             return this->hashObj->hash(this->hash + this->salt);
         }
-        ~SaltIterator() { delete this->hashObj; }
     };
 
-    std::vector<Block> chain;
-    const Hash* hash;
-    SaltIterator salt_iter;
+    std::vector<std::unique_ptr<Block>> chain;  // the chain of blocks
+    const Hash* hash;           // the hash function that is used
+    SaltIterator salt_iter;     // the salt iterator that is used to generate the salts
 
    private:
-    bool addBlock() noexcept;
+    // adds a new block to the chain
+    virtual bool addBlock() noexcept;
+    // returns the free space in the last block
     unsigned char getFreeSpaceInLastBlock() const noexcept;
 
    public:
+    // creates a new empty blockchain with the hash function, the password hash and the encrypted salt
     BlockChain(const Hash* hash, const Bytes passwordhash, const Bytes enc_salt);
     BlockChain(const BlockChain&) = delete;
     BlockChain& operator=(const BlockChain&) = delete;
     BlockChain() = delete;
 
-    virtual void addData(const Bytes data) noexcept;
+    // adds new data to the blockchain
+    void addData(const Bytes data) noexcept;
 
-    virtual Bytes getData() const;
+    // returns the result data of the blockchain
+    Bytes getResult() const;
 
-    ~BlockChain() { delete this->hash; };
+    // virtual destructor frees the resources
+    virtual ~BlockChain() { 
+        delete this->hash;
+    };
 };
---
+++ src/blockchain.cpp
@@ -0,0 +1,67 @@
+#include "blockchain.h"
+
+BlockChain::BlockChain(const Hash* hash, const Bytes passwordhash, const Bytes enc_salt){
+    // initialize the salt generator (iterator)
+    this->salt_iter.init(passwordhash, enc_salt, hash);
+    this->hash = hash;
+}
+
+void BlockChain::addData(const Bytes data) noexcept {
+    Bytes data_copy = data;
+    while(true){
+        // get the data that can be added on the last block to complete it
+        // it is the minimum of the free space in the last block and the length of the data
+        std::optional<Bytes> data_part = data_copy.popFirstBytes(std::min<unsigned char>(this->getFreeSpaceInLastBlock(), data_copy.getLen()));
+        if(!data_part.has_value()){
+            // there is no data left to add
+            break;
+        }
+        // add the data to the last block
+        this->chain.back()->addData(data_part.value());
+        // add a new block if still data is left
+        if(data_copy.getLen() != 0)
+            this->addBlock();
+        else
+            break;
+    }
+}
+
+Bytes BlockChain::getResult() const {
+    // returns the result of the blockchain by concatenating the results of all blocks
+    Bytes res{};
+    for(int i = 0; i < this->chain.size(); i++){
+        res.addBytes(this->chain[i]->getResult());
+    }
+    return res;
+}
+
+// bool BlockChain::addBlock() noexcept{
+//     if(this->getFreeSpaceInLastBlock() != 0)
+//         // there is still free space in the last block no new block is needed
+//         return false;
+    
+//     // get the next block salt
+//     Bytes next_salt;
+//     if(this->chain.size() > 0)
+//         // hashes the last block and use it to generate the next salt
+//         next_salt = this->salt_iter.next(this->hash->hash(this->chain.back()));
+//     else
+//         // no previous block, generate the next salt without a last block hash
+//         next_salt = this->salt_iter.next();
+
+//     // create the new block
+//     Block new_block(this->hash->getHashSize(), next_salt);
+
+//     // add the new block to the chain
+//     this->chain.push_back(new_block);
+
+//     return true;
+// }
+
+unsigned char BlockChain::getFreeSpaceInLastBlock() const noexcept {
+    // returns the free space in the last block
+    if(this->chain.size() > 0)
+        return this->chain.back()->getFreeSpace();
+    else
+        return 0;
+}
