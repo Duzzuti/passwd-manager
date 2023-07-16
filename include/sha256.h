fa4e6b59401963e50a3e3eb29b2def51e27de272
Duzzuti <duzzuti@gmail.com>
2023-07-16 13:57:26 +0000

    added override specifiers

--- include/sha256.h
+++ include/sha256.h
@@ -10,7 +10,7 @@ class sha256 : public Hash {
     it uses the openssl library to perform a sha256
     */
    public:
-    int getHashSize() const noexcept { return SHA256_DIGEST_LENGTH; }  // returns the length of the sha256 (32 byte)
-    Bytes hash(const Bytes bytes) const;                               // performs the sha256 on a Bytes object
-    Bytes hash(const std::string str) const;                           // performs the sha256 on a string
+    int getHashSize() const noexcept override{ return SHA256_DIGEST_LENGTH; }  // returns the length of the sha256 (32 byte)
+    Bytes hash(const Bytes bytes) const override;                               // performs the sha256 on a Bytes object
+    Bytes hash(const std::string str) const override;                           // performs the sha256 on a string
 };
--- include/sha384.h
+++ include/sha384.h
@@ -10,7 +10,7 @@ class sha384 : public Hash {
     it uses the openssl library to perform a sha384
     */
    public:
-    int getHashSize() const noexcept { return SHA384_DIGEST_LENGTH; }  // returns the length of the sha384 (48 byte)
-    Bytes hash(const Bytes bytes) const;                               // performs the sha384 on a Bytes object
-    Bytes hash(const std::string str) const;                           // performs the sha384 on a string
+    int getHashSize() const noexcept override{ return SHA384_DIGEST_LENGTH; }  // returns the length of the sha384 (48 byte)
+    Bytes hash(const Bytes bytes) const override;                               // performs the sha384 on a Bytes object
+    Bytes hash(const std::string str) const override;                           // performs the sha384 on a string
 };
