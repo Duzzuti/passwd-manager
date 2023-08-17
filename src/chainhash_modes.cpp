e3a0d0a3074c3fba46f94230fc568f4d12721a8b
Duzzuti <duzzuti@gmail.com>
2023-08-17 15:34:19 +0000

    added caller methods for timed chainhashes

--- src/chainhash_modes.cpp
+++ src/chainhash_modes.cpp
@@ -111,13 +111,75 @@ ErrorStruct<Bytes> ChainHashModes::performChainHash(const ChainHash chainh, std:
 }
 
 ErrorStruct<ChainHashResult> ChainHashModes::performChainHash(const ChainHashTimed chainh, std::shared_ptr<Hash> hash, const Bytes data) {
+    // performs a timed chainhash on bytes
     PLOG_VERBOSE << "performing chainhash timed (mode: " << +chainh.getMode() << ", runtime: " << chainh.getRunTime() << ")";
-    // WORK
-    return ErrorStruct<ChainHashResult>();
+    PwFunc pwf = PwFunc(std::move(hash));  // init the pwfunc object with the given hash function
+    std::string constant_salt{};           // init all variables we might need, because in the switch statement no variables can be declared
+    u_int64_t count_salt{};
+    u_int64_t a{};
+    u_int64_t b{};
+    u_int64_t c{};
+    TimedResult tr{};
+    switch (chainh.getMode()) {
+        case CHAINHASH_NORMAL:                                                                                          // normal chainhash
+            tr = pwf.chainhashTimed(data, chainh.getRunTime());                                                         // just use the run time
+        case CHAINHASH_CONSTANT_SALT:                                                                                   // constant salt
+            constant_salt = charVecToString(chainh.getChainHashData().getPart("S").getBytes());                         // get the salt from the datablock
+            tr = pwf.chainhashWithConstantSaltTimed(data, chainh.getRunTime(), constant_salt);                          // use the run time and the constant salt
+        case CHAINHASH_COUNT_SALT:                                                                                      // count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the start number of the salt
+            tr = pwf.chainhashWithCountSaltTimed(data, chainh.getRunTime(), count_salt);                                // use the run time and the count salt
+        case CHAINHASH_CONSTANT_COUNT_SALT:                                                                             // constant + count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the start number of the salt
+            constant_salt = charVecToString(chainh.getChainHashData().getPart("S").getBytes());                         // get the salt from the datablock
+            tr = pwf.chainhashWithCountAndConstantSaltTimed(data, chainh.getRunTime(), count_salt, constant_salt);      // use the count and constant salt
+        case CHAINHASH_QUADRATIC:                                                                                       // Quadratic count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the count salt (start number)
+            a = toLong(chainh.getChainHashData().getPart("A"));                                                         // get the a number
+            b = toLong(chainh.getChainHashData().getPart("B"));                                                         // get the b number
+            c = toLong(chainh.getChainHashData().getPart("C"));                                                         // get the c number
+            tr = pwf.chainhashWithQuadraticCountSaltTimed(data, chainh.getRunTime(), count_salt, a, b, c);              // use the count salt and a,b,c
+        default:                                                                                                        // invalid chainhash mode
+            PLOG_FATAL << "invalid chainhash mode provided (" << +chainh.getMode() << ")";
+            throw std::invalid_argument("chainhash mode does not exist");
+    }
+    // timed result contains the actual needed iterations, we have to build a new ChainHash with these iterations
+    return ErrorStruct<ChainHashResult>(ChainHashResult{ChainHash{chainh.getMode(), tr.iterations, chainh.getChainHashData()}, tr.result});
 }
 
 ErrorStruct<ChainHashResult> ChainHashModes::performChainHash(const ChainHashTimed chainh, std::shared_ptr<Hash> hash, const std::string data) {
+    // performs a timed chainhash on a string
     PLOG_VERBOSE << "performing chainhash timed (mode: " << +chainh.getMode() << ", runtime: " << chainh.getRunTime() << ")";
-    // WORK
-    return ErrorStruct<ChainHashResult>();
+    PwFunc pwf = PwFunc(std::move(hash));  // init the pwfunc object with the given hash function
+    std::string constant_salt{};           // init all variables we might need, because in the switch statement no variables can be declared
+    u_int64_t count_salt{};
+    u_int64_t a{};
+    u_int64_t b{};
+    u_int64_t c{};
+    TimedResult tr{};
+    switch (chainh.getMode()) {
+        case CHAINHASH_NORMAL:                                                                                          // normal chainhash
+            tr = pwf.chainhashTimed(data, chainh.getRunTime());                                                         // just use the run time
+        case CHAINHASH_CONSTANT_SALT:                                                                                   // constant salt
+            constant_salt = charVecToString(chainh.getChainHashData().getPart("S").getBytes());                         // get the salt from the datablock
+            tr = pwf.chainhashWithConstantSaltTimed(data, chainh.getRunTime(), constant_salt);                          // use the run time and the constant salt
+        case CHAINHASH_COUNT_SALT:                                                                                      // count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the start number of the salt
+            tr = pwf.chainhashWithCountSaltTimed(data, chainh.getRunTime(), count_salt);                                // use the run time and the count salt
+        case CHAINHASH_CONSTANT_COUNT_SALT:                                                                             // constant + count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the start number of the salt
+            constant_salt = charVecToString(chainh.getChainHashData().getPart("S").getBytes());                         // get the salt from the datablock
+            tr = pwf.chainhashWithCountAndConstantSaltTimed(data, chainh.getRunTime(), count_salt, constant_salt);      // use the count and constant salt
+        case CHAINHASH_QUADRATIC:                                                                                       // Quadratic count salt
+            count_salt = toLong(chainh.getChainHashData().getPart("SN"));                                               // get the count salt (start number)
+            a = toLong(chainh.getChainHashData().getPart("A"));                                                         // get the a number
+            b = toLong(chainh.getChainHashData().getPart("B"));                                                         // get the b number
+            c = toLong(chainh.getChainHashData().getPart("C"));                                                         // get the c number
+            tr = pwf.chainhashWithQuadraticCountSaltTimed(data, chainh.getRunTime(), count_salt, a, b, c);              // use the count salt and a,b,c
+        default:                                                                                                        // invalid chainhash mode
+            PLOG_FATAL << "invalid chainhash mode provided (" << +chainh.getMode() << ")";
+            throw std::invalid_argument("chainhash mode does not exist");
+    }
+    // timed result contains the actual needed iterations, we have to build a new ChainHash with these iterations
+    return ErrorStruct<ChainHashResult>(ChainHashResult{ChainHash{chainh.getMode(), tr.iterations, chainh.getChainHashData()}, tr.result});
 }
