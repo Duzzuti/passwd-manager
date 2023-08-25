3519d0779d47cd2df453cacbca916061bbd43ef2
Duzzuti <duzzuti@gmail.com>
2023-08-25 13:53:30 +0000

    lots of bug fix and feature adding in bytes opt

--- include/bytes_opt.h
+++ include/bytes_opt.h
@@ -15,22 +15,30 @@ class BytesOpt {
 
    public:
     BytesOpt(const int max_len);                                         // creates an empty byte array with a given maximum length
+    BytesOpt(const BytesOpt& other);                                     // copy constructor
+    BytesOpt& operator=(const BytesOpt& other);                          // copy assignment
     void fillrandom() noexcept;                                          // fills the byte array with random bytes
     void addrandom(const int num);                                       // adds random bytes to the byte array (num is the number of bytes that will be added)
     void consumeBytes(const unsigned char* bytes, const size_t len);     // set the bytes to a given value
     void addconsumeBytes(const unsigned char* bytes, const size_t len);  // adds the bytes to the current value
     unsigned char* getBytes() const noexcept;                            // getter for the byte array (by reference)
     void copyToArray(unsigned char* array, const size_t len) const;      // copys the bytes to the given array
+    void copyToBytes(BytesOpt& b) const;                                 // copys the bytes to the given Bytes object
+    void addcopyToBytes(BytesOpt& b) const;                              // adds the bytes to the given Bytes object
     size_t getLen() const noexcept;                                      // getter for the length in bytes
     size_t getMaxLen() const noexcept;                                   // getter for the maximum length in bytes
     void addByte(const unsigned char& byte);                             // adds one byte at the end of the byte array by reference
     bool isEmpty() const noexcept;                                       // returns true if there are no bytes in the array
-    u_int64_t toLong() const noexcept;                                   // returns a long that is the decimal representation of the Bytes
+    u_int64_t toLong() const;                                            // returns a long that is the decimal representation of the Bytes
     std::string toHex() const noexcept;                                  // returns a string (with 2*len chars) that is the hexadecimal representation of the Bytes
-    bool operator==(const BytesOpt& b2);                                 // returns true if the byte arrays of the two Byte objects are equal
-    BytesOpt operator+(const BytesOpt& b2);                              // performs an add elementwise (the two byte arrays are added to each other (elementwise) mod 256)
-    BytesOpt operator-(const BytesOpt& b2);                              // performs an subtract elementwise (the second byte array is subtracted from the first (elementwise) mod 256)
+    bool operator==(const BytesOpt& b2) const noexcept;                  // returns true if the byte arrays of the two Byte objects are equal
+    bool operator!=(const BytesOpt& b2) const noexcept;                  // returns true if the byte arrays of the two Byte objects are not equal
+    BytesOpt operator+(const BytesOpt& b2) const;                        // performs an add elementwise (the two byte arrays are added to each other (elementwise) mod 256)
+    BytesOpt operator-(const BytesOpt& b2) const;                        // performs an subtract elementwise (the second byte array is subtracted from the first (elementwise) mod 256)
     ~BytesOpt() {
-        if (this->bytes == nullptr) delete[] this->bytes;
+        if (this->bytes != nullptr){
+            delete[] this->bytes;
+            this->bytes = nullptr;
+        }
     };  // destructor
 };
--- src/bytes_opt.cpp
+++ src/bytes_opt.cpp
@@ -7,9 +7,48 @@
 
 BytesOpt::BytesOpt(const int max_len) {
     // creates an empty byte array with a given maximum length
+    if (max_len < 0) {
+        // invalid length given
+        PLOG_FATAL << "The provided max_len is negative (max_len: " << max_len << ")";
+        throw std::invalid_argument("The provided max_len is negative");
+    }
     this->max_len = max_len;
     this->len = 0;
     this->bytes = new unsigned char[max_len];
+}
+
+// Copy constructor
+BytesOpt::BytesOpt(const BytesOpt& other) {
+    if(this == &other){
+        // self assignment
+        return;
+    }
+    this->max_len = other.max_len;
+    this->bytes = new unsigned char[max_len];
+    if(other.bytes == nullptr){
+        PLOG_FATAL << "cannot copy nullptr";
+        throw std::invalid_argument("cannot copy nullptr");
+    }
+    std::memcpy(this->bytes, other.bytes, other.len);
+    this->len = other.len;
+}
+
+BytesOpt& BytesOpt::operator=(const BytesOpt& other) {
+    if(this == &other){
+        // self assignment
+        return *this;
+    }
+    if(other.bytes == nullptr){
+        PLOG_FATAL << "cannot copy nullptr";
+        throw std::invalid_argument("cannot copy nullptr");
+    }
+    if(max_len < other.len){
+        PLOG_FATAL << "cannot copy to a BytesOpt object with max_len " << max_len << " from a BytesOpt object with len " << other.len;
+        throw std::length_error("cannot copy to a BytesOpt object with max_len " + std::to_string(max_len) + " from a BytesOpt object with len " + std::to_string(other.len));
+    }
+    std::memcpy(this->bytes, other.bytes, other.len);
+    this->len = other.len;
+    return *this;
 }
 
 void BytesOpt::fillrandom() noexcept {
@@ -21,8 +60,8 @@ void BytesOpt::fillrandom() noexcept {
 void BytesOpt::addrandom(const int num) {
     // adds random bytes to the byte array (num is the number of bytes that will be added)
     if (this->len + num > this->max_len) {
-        PLOG_FATAL << "given bytes are too long (given: " << num << ", max: " << this->max_len << ")";
-        throw std::invalid_argument("given bytes are too long");
+        PLOG_FATAL << "cannot add " << num << " random bytes, because there is only space for " << this->max_len - this->len;
+        throw std::length_error("cannot add " + std::to_string(num) + " random bytes, because there is only space for " + std::to_string(this->max_len - this->len));
     }
     RNG::fill_random_bytes(this->bytes + this->len, num);
     this->len += num;
@@ -31,8 +70,12 @@ void BytesOpt::addrandom(const int num) {
 void BytesOpt::consumeBytes(const unsigned char* bytes, const size_t len) {
     // set the bytes to a given value
     if (len > this->max_len) {
-        PLOG_FATAL << "given bytes are too long (given: " << len << ", max: " << this->max_len << ")";
-        throw std::invalid_argument("given bytes are too long");
+        PLOG_FATAL << "cannot consume bytes with len " << len << " in a BytesOpt object with max len " << this->max_len;
+        throw std::length_error("cannot consume bytes with len " + std::to_string(len) + " in a BytesOpt object with max len " + std::to_string(this->max_len));
+    }
+    if (bytes == nullptr) {
+        PLOG_FATAL << "cannot consume nullptr";
+        throw std::invalid_argument("cannot consume nullptr");
     }
     this->len = len;
     std::memcpy(this->bytes, bytes, len);  // copy the bytes to the byte array
@@ -41,8 +84,12 @@ void BytesOpt::consumeBytes(const unsigned char* bytes, const size_t len) {
 void BytesOpt::addconsumeBytes(const unsigned char* bytes, const size_t len) {
     // adds the bytes to the current value
     if (this->len + len > this->max_len) {
-        PLOG_FATAL << "given bytes are too long (given: " << len << ", max: " << this->max_len << ")";
-        throw std::invalid_argument("given bytes are too long");
+        PLOG_FATAL << "cannot add consume bytes with len " << len << " in a BytesOpt object with free space " << this->max_len - this->len;
+        throw std::length_error("cannot add consume bytes with len " + std::to_string(len) + " in a BytesOpt object with free space " + std::to_string(this->max_len - this->len));
+    }
+    if (bytes == nullptr) {
+        PLOG_FATAL << "cannot add consume nullptr";
+        throw std::invalid_argument("cannot add consume nullptr");
     }
     std::memcpy(this->bytes + this->len, bytes, len);  // copy the bytes to the byte array
     this->len += len;
@@ -56,10 +103,34 @@ unsigned char* BytesOpt::getBytes() const noexcept {
 void BytesOpt::copyToArray(unsigned char* array, const size_t len) const {
     // copys the bytes to the given array
     if (len < this->len) {
-        PLOG_FATAL << "given array is too short (given: " << len << ", needed: " << this->len << ")";
-        throw std::invalid_argument("given array is too short");
+        PLOG_FATAL << "given array is too short to copy all bytes in (given: " << len << ", needed: " << this->len << ")";
+        throw std::length_error("given array is too short to copy all bytes in (given: " + std::to_string(len) + ", needed: " + std::to_string(this->len) + ")");
+    }
+    if(array == nullptr){
+        PLOG_FATAL << "cannot copy to nullptr array";
+        throw std::invalid_argument("cannot copy to nullptr array");
     }
     std::memcpy(array, this->bytes, this->len);
+}
+
+void BytesOpt::copyToBytes(BytesOpt& b) const {
+    // copys the bytes to the given Bytes object
+    if (b.max_len < this->len) {
+        PLOG_FATAL << "given Bytes object is too short to copy to (given: " << b.max_len << ", needed: " << this->len << ")";
+        throw std::length_error("given Bytes object is too short to copy to (given: " + std::to_string(b.max_len) + ", needed: " + std::to_string(this->len) + ")");
+    }
+    std::memcpy(b.bytes, this->bytes, this->len);
+    b.len = this->len;
+}
+
+void BytesOpt::addcopyToBytes(BytesOpt& b) const {
+    // adds the bytes to the given Bytes object
+    if (b.max_len < this->len + b.len) {
+        PLOG_FATAL << "given Bytes object is too short to add copy to (given: " << b.max_len << ", needed: " << this->len << ")";
+        throw std::length_error("given Bytes object is too short to add copy to (given: " + std::to_string(b.max_len) + ", needed: " + std::to_string(this->len) + ")");
+    }
+    std::memcpy(b.bytes + b.len, this->bytes, this->len);
+    b.len += this->len;
 }
 
 size_t BytesOpt::getLen() const noexcept {
@@ -74,9 +145,9 @@ size_t BytesOpt::getMaxLen() const noexcept {
 
 void BytesOpt::addByte(const unsigned char& byte) {
     // adds one byte at the end of the byte array by reference
-    if (this->len + 1 > this->max_len) {
-        PLOG_FATAL << "given bytes are too long (given: " << 1 << ", max: " << this->max_len << ")";
-        throw std::invalid_argument("given bytes are too long");
+    if (this->len >= this->max_len) {
+        PLOG_FATAL << "Bytes are already full. Cannot add an other byte (max_len = len = " << this->max_len << ")";
+        throw std::invalid_argument("Bytes are already full. Cannot add an other byte (max_len = len = " + std::to_string(this->max_len) + ")");
     }
     this->bytes[this->len] = byte;
     this->len++;
@@ -87,13 +158,19 @@ bool BytesOpt::isEmpty() const noexcept {
     return this->len == 0;
 }
 
-bool BytesOpt::operator==(const BytesOpt& b2) {
+bool BytesOpt::operator==(const BytesOpt& b2) const noexcept{
     // returns true if the byte arrays of the two Byte objects are equal
     if (this->len != b2.len) return false;
     return std::memcmp(this->bytes, b2.bytes, this->len) == 0;
 }
 
-BytesOpt BytesOpt::operator+(const BytesOpt& b2) {
+bool BytesOpt::operator!=(const BytesOpt& b2) const noexcept { 
+    // returns true if the byte arrays of the two Byte objects are not equal
+    if (this->len != b2.len) return true;
+    return std::memcmp(this->bytes, b2.bytes, this->len) != 0;
+}
+
+BytesOpt BytesOpt::operator+(const BytesOpt& b2) const {
     // performs an add elementwise (the two byte arrays are added to each other (elementwise) mod 256)
     if (this->len != b2.len) {
         PLOG_FATAL << "cannot add bytes with different lengths (len1: " << this->len << ", len2: " << b2.len << ")";
@@ -107,7 +184,7 @@ BytesOpt BytesOpt::operator+(const BytesOpt& b2) {
     return res;
 }
 
-BytesOpt BytesOpt::operator-(const BytesOpt& b2) {
+BytesOpt BytesOpt::operator-(const BytesOpt& b2) const {
     // performs an subtract elementwise (the second byte array is subtracted from the first (elementwise) mod 256)
     if (this->len != b2.len) {
         PLOG_FATAL << "cannot subtract bytes with different lengths (len1: " << this->len << ", len2: " << b2.len << ")";
@@ -135,11 +212,13 @@ std::string BytesOpt::toHex() const noexcept {
     return hexString;
 }
 
-u_int64_t BytesOpt::toLong() const noexcept {
+u_int64_t BytesOpt::toLong() const {
     // returns a long that is the decimal representation of the Bytes
-    u_int64_t res = 0;
-    for (size_t i = 0; i < this->len; i++) {
-        res += this->bytes[i] << (8 * i);
+    if(this->len > sizeof(u_int64_t)){
+        PLOG_ERROR << "BytesOpt::toLong() is not defined for BytesOpt with more than 8 bytes";
+        throw std::runtime_error("BytesOpt::toLong() is not defined for BytesOpt with more than 8 bytes");
     }
+    u_int64_t res;
+    std::memcpy(&res, this->bytes, this->len);
     return res;
 }
