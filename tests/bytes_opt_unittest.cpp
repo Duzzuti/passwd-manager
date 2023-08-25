54c56deb3ea0dd44d7398e2061ac2b9af38a7c6e
Duzzuti <duzzuti@gmail.com>
2023-08-25 18:03:04 +0000

    finish bytes opt unittest

--- tests/bytes_opt_unittest.cpp
+++ tests/bytes_opt_unittest.cpp
@@ -780,3 +780,418 @@ TEST(BytesOptClass, copy) {
     EXPECT_TRUE(std::memcmp(b5.getBytes(), bytes2, 10) == 0);
     EXPECT_FALSE(std::memcmp(b5.getBytes(), bytes2, 20) == 0);
 }
+
+TEST(BytesOptClass, addByte){
+    BytesOpt b1(10);
+    b1.addByte(0xad);
+    EXPECT_EQ(1, b1.getLen());
+    EXPECT_EQ(10, b1.getMaxLen());
+    EXPECT_EQ(0xad, b1.getBytes()[0]);
+
+    BytesOpt b3(10);
+    b3.addByte(0xad);
+    b3.addByte(0xac);
+    EXPECT_EQ(2, b3.getLen());
+    EXPECT_EQ(10, b3.getMaxLen());
+    EXPECT_EQ(0xad, b3.getBytes()[0]);
+    EXPECT_EQ(0xac, b3.getBytes()[1]);
+
+    BytesOpt b5(256);
+    for(int i = 0; i < 256; i++){
+        b5.addByte(i);
+        EXPECT_EQ(i + 1, b5.getLen());
+        EXPECT_EQ(256, b5.getMaxLen());
+    }
+    for(int i = 0; i < 256; i++){
+        EXPECT_EQ(i, b5.getBytes()[i]);
+    }
+
+    // exception checks
+    BytesOpt b6(0);
+    EXPECT_THROW(b6.addByte(0xad), std::length_error);
+    BytesOpt b7(10);
+    EXPECT_NO_THROW(b7.addByte(0xad));
+    BytesOpt b8(255);
+    
+    for(int i = 0; i < 256; i++){
+        if(i < 255){
+            EXPECT_NO_THROW(b8.addByte(i));
+        }else{
+            EXPECT_THROW(b8.addByte(i), std::length_error);
+        }
+    }
+}
+
+TEST(BytesOptClass, isEmpty){
+    BytesOpt b1(10);
+    EXPECT_TRUE(b1.isEmpty());
+    b1.addByte(0xad);
+    EXPECT_FALSE(b1.isEmpty());
+    b1.fillrandom();
+    EXPECT_FALSE(b1.isEmpty());
+
+    BytesOpt b2(0);
+    EXPECT_TRUE(b2.isEmpty());
+}
+
+TEST(BytesOptClass, toLong){
+    BytesOpt b0(8);
+    EXPECT_EQ(0, b0.toLong());
+    BytesOpt b1(0);
+    EXPECT_EQ(0, b1.toLong());
+    BytesOpt b2(1);
+    b2.addByte(0xad);
+    EXPECT_EQ(0xad, b2.toLong());
+    BytesOpt b3(2);
+    b3.addByte(0xad);
+    b3.addByte(0xac);
+    EXPECT_EQ(0xadac, b3.toLong());
+    BytesOpt b4(3);
+    b4.addByte(0xad);
+    b4.addByte(0xac);
+    b4.addByte(0xab);
+    EXPECT_EQ(0xadacab, b4.toLong());
+    BytesOpt b5(4);
+    b5.addByte(0xad);
+    b5.addByte(0xac);
+    b5.addByte(0xab);
+    b5.addByte(0xaa);
+    EXPECT_EQ(0xadacabaa, b5.toLong());
+    BytesOpt b6(5);
+    b6.addByte(0x00);
+    b6.addByte(0x00);
+    b6.addByte(0x00);
+    b6.addByte(0x11);
+    b6.addByte(0x22);
+    EXPECT_EQ(0x0000001122, b6.toLong());
+    BytesOpt b7(6);
+    b7.addByte(0x45);
+    b7.addByte(0x00);
+    b7.addByte(0x00);
+    b7.addByte(0x00);
+    b7.addByte(0x11);
+    b7.addByte(0x22);
+    EXPECT_EQ(0x450000001122, b7.toLong());
+    BytesOpt b8(7);
+    b8.addByte(0x00);
+    b8.addByte(0xff);
+    b8.addByte(0xff);
+    b8.addByte(0xff);
+    b8.addByte(0xff);
+    b8.addByte(0xff);
+    b8.addByte(0xff);
+    EXPECT_EQ(0x00ffffffffffff, b8.toLong());
+    BytesOpt b9(8);
+    b9.addByte(0xff);
+    b9.addByte(0xfe);
+    b9.addByte(0xfd);
+    b9.addByte(0xfc);
+    b9.addByte(0xfb);
+    b9.addByte(0xfa);
+    b9.addByte(0xf9);
+    b9.addByte(0xf8);   
+    EXPECT_EQ(0xfffefdfcfbfaf9f8, b9.toLong());
+
+    // exception checks
+    BytesOpt b10(9);
+    b10.fillrandom();
+    EXPECT_THROW(b10.toLong(), std::length_error);
+
+    BytesOpt b11(9);
+    b11.addrandom(8);
+    EXPECT_NO_THROW(b11.toLong());
+
+    BytesOpt b12(10);
+    b12.addrandom(9);
+    EXPECT_THROW(b12.toLong(), std::length_error);
+}
+
+TEST(BytesOptClass, toHex){
+    BytesOpt b1(0);
+    EXPECT_EQ("", b1.toHex());
+    BytesOpt b2(1);
+    b2.addByte(0xad);
+    EXPECT_EQ("AD", b2.toHex());
+    BytesOpt b3(2);
+    b3.addByte(0xad);
+    b3.addByte(0xac);
+    EXPECT_EQ("ADAC", b3.toHex());
+    BytesOpt b4(3);
+    b4.addByte(0xad);
+    b4.addByte(0xac);
+    b4.addByte(0xab);
+    EXPECT_EQ("ADACAB", b4.toHex());
+    BytesOpt b5(4);
+    b5.addByte(0xad);
+    b5.addByte(0xac);
+    b5.addByte(0xab);
+    b5.addByte(0xaa);
+    EXPECT_EQ("ADACABAA", b5.toHex());
+    BytesOpt b6(5);
+    b6.addByte(0x00);
+    b6.addByte(0x00);
+    b6.addByte(0x00);
+    b6.addByte(0x11);
+    b6.addByte(0x22);
+    EXPECT_EQ("0000001122", b6.toHex());
+    BytesOpt b7(6);
+    b7.addByte(0x45);
+    b7.addByte(0x00);
+    b7.addByte(0x00);
+    b7.addByte(0x00);
+    b7.addByte(0x11);
+    b7.addByte(0x22);
+    EXPECT_EQ("450000001122", b7.toHex());
+
+    BytesOpt b8(256);
+    for(int i = 0; i < 256; i++){
+        b8.addByte(i);
+    }
+    std::string hex = b8.toHex();
+    for(int i = 0; i < 256; i++){
+        std::stringstream ss;
+        ss << std::uppercase << std::hex << i;
+        std::string hex2 = ss.str();
+        if(hex2.length() == 1){
+            hex2 = "0" + hex2;
+        }
+        EXPECT_EQ(hex2, hex.substr(i * 2, 2));
+    }
+}
+
+TEST(BytesOptClass, fromLong){
+    u_int64_t l1 = 0;
+    BytesOpt b1 = BytesOpt::fromLong(l1);
+    EXPECT_EQ(0, b1.getLen());
+    EXPECT_EQ(8, b1.getMaxLen());
+    EXPECT_EQ(0, b1.toLong());
+
+    u_int64_t l2 = 0xad;
+    BytesOpt b2 = BytesOpt::fromLong(l2);
+    EXPECT_EQ(1, b2.getLen());
+    EXPECT_EQ(8, b2.getMaxLen());
+    EXPECT_EQ(0xad, b2.toLong());
+
+    u_int64_t l3 = 0xadac;
+    BytesOpt b3 = BytesOpt::fromLong(l3);
+    EXPECT_EQ(2, b3.getLen());
+    EXPECT_EQ(8, b3.getMaxLen());
+    EXPECT_EQ(0xadac, b3.toLong());
+
+    u_int64_t l4 = 0xadacab;
+    BytesOpt b4 = BytesOpt::fromLong(l4);
+    EXPECT_EQ(3, b4.getLen());
+    EXPECT_EQ(8, b4.getMaxLen());
+    EXPECT_EQ(0xadacab, b4.toLong());
+
+    u_int64_t l5 = 0x00ac00aa;
+    BytesOpt b5 = BytesOpt::fromLong(l5);
+    EXPECT_EQ(3, b5.getLen());
+    EXPECT_EQ(8, b5.getMaxLen());
+    EXPECT_EQ(0xac00aa, b5.toLong());
+
+    u_int64_t l6 = 0x0000001122;
+    BytesOpt b6 = BytesOpt::fromLong(l6);
+    EXPECT_EQ(2, b6.getLen());
+    EXPECT_EQ(8, b6.getMaxLen());
+    EXPECT_EQ(0x1122, b6.toLong());
+
+    u_int64_t l7 = 0x450000001122;
+    BytesOpt b7 = BytesOpt::fromLong(l7);
+    EXPECT_EQ(6, b7.getLen());
+    EXPECT_EQ(8, b7.getMaxLen());
+    EXPECT_EQ(0x450000001122, b7.toLong());
+
+    u_int64_t l8 = 0x00ffffffffffff;
+    BytesOpt b8 = BytesOpt::fromLong(l8);
+    EXPECT_EQ(6, b8.getLen());
+    EXPECT_EQ(8, b8.getMaxLen());
+    EXPECT_EQ(0xffffffffffff, b8.toLong());
+
+    u_int64_t l9 = 0xfffefdfcfbfaf9f8;
+    BytesOpt b9 = BytesOpt::fromLong(l9);
+    EXPECT_EQ(8, b9.getLen());
+    EXPECT_EQ(8, b9.getMaxLen());
+    EXPECT_EQ(0xfffefdfcfbfaf9f8, b9.toLong());
+
+    u_int64_t l10 = 0xfffefdfcfbfaf9f;
+    BytesOpt b10 = BytesOpt::fromLong(l10);
+    EXPECT_EQ(8, b10.getLen());
+    EXPECT_EQ(8, b10.getMaxLen());
+    EXPECT_EQ(0xfffefdfcfbfaf9f, b10.toLong());
+
+    // exception checks
+    u_int64_t l15 = 0x00fffefdfcfbfaf9f8;
+    EXPECT_NO_THROW(BytesOpt b15 = BytesOpt::fromLong(l15));
+
+    u_int64_t l16 = 0x0fffefdfcfbfaf9f;
+    EXPECT_NO_THROW(BytesOpt b16 = BytesOpt::fromLong(l16));
+
+    u_int64_t l17 = 0x0fffefdfcfbfaf9f8;
+    EXPECT_NO_THROW(BytesOpt b17 = BytesOpt::fromLong(l17));
+}
+
+TEST(BytesOptClass, equal){
+    BytesOpt b1(10);
+    BytesOpt b2(8);
+    EXPECT_EQ(b1, b2);
+    b1.addByte(0xad);
+    EXPECT_NE(b1, b2);
+    b2.addByte(0xad);
+    EXPECT_EQ(b1, b2);
+    b1.addByte(0xac);
+    EXPECT_NE(b1, b2);
+    b2.addByte(0xac);
+    EXPECT_EQ(b1, b2);
+    b1.fillrandom();
+    b2.fillrandom();
+    EXPECT_NE(b1, b2);
+
+    BytesOpt b3(0);
+    BytesOpt b4(10000);
+    EXPECT_EQ(b3, b4);
+    b4.addByte(0xad);
+    EXPECT_NE(b3, b4);
+
+    unsigned char bytes1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    BytesOpt b5(15);
+    BytesOpt b6(11);
+    EXPECT_EQ(b5, b6);
+    b5.consumeBytes(bytes1, 10);
+    EXPECT_NE(b5, b6);
+    b6.consumeBytes(bytes1, 10);
+    EXPECT_EQ(b5, b6);
+}
+
+TEST(BytesOptClass, plus){
+    for(int i = 0; i < 1000; i++){
+        BytesOpt b1(10);
+        BytesOpt b2(10);
+        b1.fillrandom();
+        b2.fillrandom();
+        BytesOpt b3 = b1 + b2;
+        EXPECT_EQ(b1.getLen(), b3.getLen());
+        EXPECT_EQ(b2.getLen(), b3.getLen());
+        EXPECT_EQ(10, b3.getMaxLen());
+        
+        for(int j = 0; j < 10; j++){
+            EXPECT_EQ((unsigned char)(b1.getBytes()[j] + b2.getBytes()[j]), b3.getBytes()[j]);
+        }
+    }
+    for(int i = 0; i < 1000; i++){
+        BytesOpt b1(5);
+        BytesOpt b2(10);
+        b1.addrandom(5);
+        b2.addrandom(5);
+        BytesOpt b3 = b1 + b2;
+        EXPECT_EQ(b1.getLen(), b3.getLen());
+        EXPECT_EQ(b2.getLen(), b3.getLen());
+        EXPECT_EQ(5, b3.getMaxLen());
+        
+        for(int j = 0; j < 5; j++){
+            EXPECT_EQ((unsigned char)(b1.getBytes()[j] + b2.getBytes()[j]), b3.getBytes()[j]);
+        }
+    }
+    BytesOpt b1(0);
+    BytesOpt b2(0);
+    BytesOpt b3 = b1 + b2;
+    EXPECT_EQ(b1.getLen(), b3.getLen());
+    EXPECT_EQ(b2.getLen(), b3.getLen());
+    EXPECT_EQ(0, b3.getMaxLen());
+
+    unsigned char bytes1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    unsigned char bytes2[10] = {126, 255, 255, 34, 17, 0, 0, 0, 0, 253};
+    unsigned char bytes3[10] = {126, 0, 1, 37, 21, 5, 6, 7, 8, 6};
+
+    BytesOpt b4(10);
+    BytesOpt b5(10);
+    b4.consumeBytes(bytes1, 10);
+    b5.consumeBytes(bytes2, 10);
+    BytesOpt b6 = b4 + b5;
+    EXPECT_TRUE(std::memcmp(b6.getBytes(), bytes3, 10) == 0);
+
+    // exception checks
+    BytesOpt b7(11);
+    BytesOpt b8(10);
+    b7.fillrandom();
+    EXPECT_THROW(b7 + b8, std::length_error);
+    b8.fillrandom();
+    EXPECT_THROW(b7 + b8, std::length_error);
+    BytesOpt b9(2);
+    BytesOpt b10(10);
+    b9.fillrandom();
+    b10.addrandom(2);
+    EXPECT_NO_THROW(b9 + b10);
+    BytesOpt b11(0);
+    BytesOpt b12(10);
+    EXPECT_NO_THROW(b11 + b12);
+    b12.addByte(0xad);
+    EXPECT_THROW(b11 + b12, std::length_error);
+
+}
+
+TEST(BytesOptClass, minus){
+    for(int i = 0; i < 1000; i++){
+        BytesOpt b1(10);
+        BytesOpt b2(10);
+        b1.fillrandom();
+        b2.fillrandom();
+        BytesOpt b3 = b1 - b2;
+        EXPECT_EQ(b1.getLen(), b3.getLen());
+        EXPECT_EQ(b2.getLen(), b3.getLen());
+        EXPECT_EQ(10, b3.getMaxLen());
+        
+        for(int j = 0; j < 10; j++){
+            EXPECT_EQ((unsigned char)(b1.getBytes()[j] - b2.getBytes()[j]), b3.getBytes()[j]);
+        }
+    }
+    for(int i = 0; i < 1000; i++){
+        BytesOpt b1(5);
+        BytesOpt b2(10);
+        b1.addrandom(5);
+        b2.addrandom(5);
+        BytesOpt b3 = b1 - b2;
+        EXPECT_EQ(b1.getLen(), b3.getLen());
+        EXPECT_EQ(b2.getLen(), b3.getLen());
+        EXPECT_EQ(5, b3.getMaxLen());
+        
+        for(int j = 0; j < 5; j++){
+            EXPECT_EQ((unsigned char)(b1.getBytes()[j] - b2.getBytes()[j]), b3.getBytes()[j]);
+        }
+    }
+    BytesOpt b1(0);
+    BytesOpt b2(0);
+    BytesOpt b3 = b1 - b2;
+    EXPECT_EQ(b1.getLen(), b3.getLen());
+    EXPECT_EQ(b2.getLen(), b3.getLen());
+    EXPECT_EQ(0, b3.getMaxLen());
+
+    unsigned char bytes1[10] = {126, 0, 1, 37, 21, 5, 6, 7, 8, 6};
+    unsigned char bytes2[10] = {126, 255, 255, 34, 17, 0, 0, 0, 0, 253};
+    unsigned char bytes3[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    BytesOpt b4(10);
+    BytesOpt b5(10);
+    b4.consumeBytes(bytes1, 10);
+    b5.consumeBytes(bytes2, 10);
+    BytesOpt b6 = b4 - b5;
+    EXPECT_TRUE(std::memcmp(b6.getBytes(), bytes3, 10) == 0);
+
+    // exception checks
+    BytesOpt b7(11);
+    BytesOpt b8(10);
+    b7.fillrandom();
+    EXPECT_THROW(b7 - b8, std::length_error);
+    b8.fillrandom();
+    EXPECT_THROW(b7 - b8, std::length_error);
+    BytesOpt b9(2);
+    BytesOpt b10(10);
+    b9.fillrandom();
+    b10.addrandom(2);
+    EXPECT_NO_THROW(b9 - b10);
+    BytesOpt b11(0);
+    BytesOpt b12(10);
+    EXPECT_NO_THROW(b11 - b12);
+    b12.addByte(0xad);
+    EXPECT_THROW(b11 - b12, std::length_error);
+}
