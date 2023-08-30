ae4310f0f3909c622ecab92b99cff8c5f095f92a
Duzzuti <duzzuti@gmail.com>
2023-08-30 16:01:38 +0000

    optimized format

--- include/format.h
+++ include/format.h
@@ -13,15 +13,22 @@ class Format {
     chainhash_modes.md
     */
    private:
-    std::string format;      // the format string depending on the chainhash mode
-    CHModes chainhash_mode;  // the chainhash mode that the user provides with the constructor
+    const CHModes chainhash_mode;  // the chainhash mode that the user provides with the constructor
+    const std::string format;      // the format string depending on the chainhash mode
+    const std::vector<NameLen> name_lens;  // a list of all expected datablock parts as NameLen
+   private:
+    // calculates the format list for a given chainhash mode
+    std::string getFormatString(const CHModes& chainhash_mode) const;  // gets the format string for a given chainhash mode
+    // calculates the NameLen list for a given chainhash mode
+    std::vector<NameLen> calcNameLenList(const CHModes& chainhash_mode) const;
    public:
     // constructor has to set the chainhash_mode and the corresponding string
+    Format& operator=(const Format& format) = delete;  // copy assignment is deleted
     Format(const CHModes chainhash_mode);
 
     // getter for the chainhash mode
     CHModes getChainMode() const noexcept;
-    // calculates a list of NameLens, these structs contain the name and length of each datablock part
+    // gets the list of NameLens, these structs contain the name and length of each datablock part
     std::vector<NameLen> getNameLenList() const;
 
     // it is possible to compare two formats (depending on their format strings)
