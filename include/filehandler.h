d468f9b1682e9f6c7fabff27f29f41c2ea283e61
Duzzuti <duzzuti@gmail.com>
2023-09-03 12:49:08 +0000

    optimized filehandler

--- include/filehandler.h
+++ include/filehandler.h
@@ -11,20 +11,23 @@ class FileHandler {
     That means it writes and reads from a given enc file
     */
    private:
-    std::filesystem::path filepath;  // stores the path to the encryption file
+    const std::filesystem::path filepath;  // stores the path to the encryption file
+    u_int64_t header_size = 0;      // stores the size of the data header
    public:
     static const std::string extension;  // encryption files (.enc)
    public:
-    static ErrorStruct<bool> isValidPath(std::filesystem::path file, bool should_exist) noexcept;  // checks if the path is valid
-    static ErrorStruct<bool> createFile(std::filesystem::path file) noexcept;                      // creates a file
-    FileHandler(std::filesystem::path file);                                                       // sets the filepath
-    ErrorStruct<bool> isDataHeader(FModes exp_file_mode) const noexcept;                           // checks if the file has a valid data header
-    bool isEmtpy() const;                                                                          // checks if the file is empty
-    ErrorStruct<DataHeader> getDataHeader() const noexcept;                                        // reads the data header from the file
-    ErrorStruct<Bytes> getData() const noexcept;                                                   // reads the data from the file (without the data header)
-    Bytes getFirstBytes(const int num) const;                                                      // reads the first num Bytes from the encryption file
-    Bytes getAllBytes() const;                                                                     // reads all Bytes from the given encryption file
-    ErrorStruct<bool> writeBytesIfEmpty(Bytes bytes) const noexcept;                               // writes the given bytes to the file if it is empty
-    ErrorStruct<bool> writeBytes(Bytes bytes) const noexcept;                                      // writes the given bytes to the file
-    std::filesystem::path getPath() const noexcept;                                                // returns the filepath
+    static ErrorStruct<bool> isValidPath(const std::filesystem::path& file, bool should_exist) noexcept;  // checks if the path is valid
+    static ErrorStruct<bool> createFile(const std::filesystem::path& file) noexcept;                      // creates a file
+    FileHandler(const std::filesystem::path& file);                                                       // sets the filepath
+    ErrorStruct<bool> isDataHeader(FModes exp_file_mode) noexcept;                                        // checks if the file has a valid data header
+    bool isEmtpy() const noexcept;                                                                        // checks if the file is empty
+    ErrorStruct<DataHeader> getDataHeader() noexcept;                                                     // reads the data header from the file
+    ErrorStruct<std::ifstream> getData() noexcept;                                                        // reads the data from the file (without the data header)
+    Bytes getFirstBytes(const size_t num) const;                                                          // reads the first num Bytes from the encryption file
+    Bytes getAllBytes() const;                                                                            // reads all Bytes from the given encryption file
+    ErrorStruct<bool> writeBytesIfEmpty(Bytes& bytes) noexcept;                                           // writes the given bytes to the file if it is empty
+    ErrorStruct<bool> writeBytes(Bytes& bytes) noexcept;                                                  // writes the given bytes to the file
+    ErrorStruct<std::ofstream> getWriteStream() noexcept;                                                 // returns a write stream to the file
+    ErrorStruct<std::ofstream> getWriteStreamIfEmpty() noexcept;                                          // returns a write stream to the file if it is empty
+    std::filesystem::path getPath() const noexcept;                                                       // returns the filepath
 };
--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -10,7 +10,7 @@ implements the FileHandler class
 
 const std::string FileHandler::extension = ".enc";
 
-ErrorStruct<bool> FileHandler::isValidPath(std::filesystem::path file, bool should_exist) noexcept {
+ErrorStruct<bool> FileHandler::isValidPath(const std::filesystem::path& file, bool should_exist) noexcept {
     // checks if the given file path is valid (file_path has to be not empty and have the right extension)
     // if should_exist is true, the file has to exist otherwise it has to not exist
     ErrorStruct<bool> err;
@@ -54,7 +54,7 @@ ErrorStruct<bool> FileHandler::isValidPath(std::filesystem::path file, bool shou
     return ErrorStruct<bool>{true};
 }
 
-ErrorStruct<bool> FileHandler::createFile(std::filesystem::path file) noexcept {
+ErrorStruct<bool> FileHandler::createFile(const std::filesystem::path& file) noexcept {
     // creates a new .enc file at the given path and validates it
     PLOG_VERBOSE << "Creating new file (file_path: " << file << ")";
     ErrorStruct<bool> err;
@@ -67,7 +67,7 @@ ErrorStruct<bool> FileHandler::createFile(std::filesystem::path file) noexcept {
         return err2;
     }
     // create the file
-    std::ofstream file_stream{file};
+    std::ofstream file_stream{file.c_str()};
     if (!file_stream.is_open() || !file_stream) {
         // the file could not be created
         PLOG_ERROR << "The file could not be created (createFile)";
@@ -79,17 +79,15 @@ ErrorStruct<bool> FileHandler::createFile(std::filesystem::path file) noexcept {
     return ErrorStruct<bool>{true};
 }
 
-FileHandler::FileHandler(std::filesystem::path file) {
+FileHandler::FileHandler(const std::filesystem::path& file) : filepath(file) {
     // sets the filepath
-    if (FileHandler::isValidPath(file, true).isSuccess()) {
-        this->filepath = file;
-        return;
+    if (!FileHandler::isValidPath(file, true).isSuccess()) {
+        PLOG_FATAL << "The given file path is invalid (file path: " << file << ")";
+        throw std::runtime_error("The given file path is invalid (file path: " + file.string() + ")");
     }
-    PLOG_FATAL << "The given file path is invalid (file path: " << file << ")";
-    throw std::runtime_error("The given file path is invalid (file path: " + file.string() + ")");
 }
 
-ErrorStruct<bool> FileHandler::isDataHeader(FModes exp_file_mode) const noexcept {
+ErrorStruct<bool> FileHandler::isDataHeader(FModes exp_file_mode) noexcept {
     ErrorStruct<DataHeader> err = this->getDataHeader();
     if (!err.isSuccess()) {
         // the data header could not be read
@@ -99,12 +97,12 @@ ErrorStruct<bool> FileHandler::isDataHeader(FModes exp_file_mode) const noexcept
         // the data header was read successfully and the file mode is correct
         return ErrorStruct<bool>{true};
     }
-    PLOG_WARNING << "The data header was read successfully but the file mode is incorrect (isDataHeader) (file path: " << this->filepath << ", expected file mode: " << exp_file_mode
-                 << ", actual file mode: " << err.returnValue().getDataHeaderParts().getFileDataMode() << ")";
+    PLOG_WARNING << "The data header was read successfully but the file mode is incorrect (isDataHeader) (file path: " << this->filepath << ", expected file mode: " << +exp_file_mode
+                 << ", actual file mode: " << +err.returnValue().getDataHeaderParts().getFileDataMode() << ")";
     return ErrorStruct<bool>{FAIL, ERR_FILEMODE_INVALID, std::to_string(+err.returnValue().getDataHeaderParts().getFileDataMode())};
 }
 
-bool FileHandler::isEmtpy() const {
+bool FileHandler::isEmtpy() const noexcept{
     // checks if the file is empty
     try {
         // try to read the first byte
@@ -116,27 +114,37 @@ bool FileHandler::isEmtpy() const {
     return false;
 }
 
-ErrorStruct<DataHeader> FileHandler::getDataHeader() const noexcept {
+ErrorStruct<DataHeader> FileHandler::getDataHeader() noexcept {
     // reads the data header from the file
-    Bytes content = this->getAllBytes();
-    return DataHeader::setHeaderBytes(content);
+    std::ifstream filestream(this->filepath.c_str(), std::ios::binary);
+    ErrorStruct<DataHeader> ret = DataHeader::setHeaderBytes(filestream);
+    header_size = ret.returnValue().getHeaderLength();
+    filestream.close();
+    return ret;
 }
 
-ErrorStruct<Bytes> FileHandler::getData() const noexcept {
+ErrorStruct<std::ifstream> FileHandler::getData() noexcept {
     // reads the data from the file (without the data header)
-    Bytes content = this->getAllBytes();
-    ErrorStruct<DataHeader> err = DataHeader::setHeaderBytes(content);
-    if (!err.isSuccess()) {
-        // the data header could not be read
-        PLOG_ERROR << "The data header could not be read (getData) (errorCode: " << +err.errorCode << ", errorInfo: " << err.errorInfo << ", what: " << err.what << ")";
-        return ErrorStruct<Bytes>{err.success, err.errorCode, err.errorInfo, err.what};
+    std::ifstream file(this->filepath.c_str(), std::ios::binary);
+    if(header_size != 0){
+        file.seekg(header_size, std::ios::beg);
+        return ErrorStruct<std::ifstream>{std::move(file)};
+    }else{
+        ErrorStruct<DataHeader> err = DataHeader::setHeaderBytes(file);
+        if (!err.isSuccess()) {
+            // the data header could not be read
+            file.close();
+            PLOG_ERROR << "The data header could not be read (getData) (errorCode: " << +err.errorCode << ", errorInfo: " << err.errorInfo << ", what: " << err.what << ")";
+            return ErrorStruct<std::ifstream>{err.success, err.errorCode, err.errorInfo, err.what};
+        }
+        header_size = err.returnValue().getHeaderLength();
+        // the data header was read successfully
+        // the remaining bytes are the data
+        return ErrorStruct<std::ifstream>{std::move(file)};
     }
-    // the data header was read successfully
-    // the remaining bytes are the data
-    return ErrorStruct<Bytes>{content};
 }
 
-ErrorStruct<bool> FileHandler::writeBytes(Bytes bytes) const noexcept {
+ErrorStruct<bool> FileHandler::writeBytes(Bytes& bytes) noexcept {
     // writes bytes to the file
     // overrides old content
     PLOG_VERBOSE << "Writing to file (file_path: " << this->filepath.c_str() << ")";
@@ -147,26 +155,56 @@ ErrorStruct<bool> FileHandler::writeBytes(Bytes bytes) const noexcept {
         return err_file;
     }
     // file path is valid
-    std::ofstream file(this->filepath, std::ios::binary);
+    std::ofstream file(this->filepath.c_str(), std::ios::binary);
     if (!file.is_open()) {
         PLOG_FATAL << "The file could not be opened";
         // should not happen because the file was checked before
         return ErrorStruct<bool>{SuccessType::FAIL, ErrorCode::ERR_FILE_NOT_OPEN, this->filepath.c_str()};
     }
     // write the data
-    unsigned char* data = new unsigned char[bytes.getLen()];
-    bytes.getBytesArray(data, bytes.getLen());
-    file.write(reinterpret_cast<char*>(data), bytes.getLen());
+    file.write(reinterpret_cast<char*>(bytes.getBytes()), bytes.getLen());
     file.close();
+    this->header_size = 0;
 
-    delete[] data;
     PLOG_VERBOSE << "Successfully wrote " << bytes.getLen() << " bytes to file (file_path: " << this->filepath.c_str() << ")";
     return ErrorStruct<bool>{true};
 }
 
+ErrorStruct<std::ofstream> FileHandler::getWriteStream() noexcept {
+    this->header_size = 0;
+    // returns a write stream to the file
+    // overrides old content
+    PLOG_VERBOSE << "Getting write stream to file (file_path: " << this->filepath.c_str() << ")";
+    // checks if the selected file exists
+    ErrorStruct<bool> err_file = this->isValidPath(this->filepath, true);
+    if (!err_file.isSuccess()) {
+        PLOG_ERROR << "The provided file path is invalid (getWriteStream) (errorCode: " << +err_file.errorCode << ", errorInfo: " << err_file.errorInfo << ", what: " << err_file.what << ")";
+        return ErrorStruct<std::ofstream>{err_file.success, err_file.errorCode, err_file.errorInfo, err_file.what};
+    }
+    // file path is valid
+    std::ofstream file(this->filepath.c_str(), std::ios::binary);
+    if (!file.is_open()) {
+        PLOG_FATAL << "The file could not be opened";
+        // should not happen because the file was checked before
+        return ErrorStruct<std::ofstream>{SuccessType::FAIL, ErrorCode::ERR_FILE_NOT_OPEN, this->filepath.c_str()};
+    }
+    return ErrorStruct<std::ofstream>{std::move(file)};
+}
+
+ErrorStruct<std::ofstream> FileHandler::getWriteStreamIfEmpty() noexcept {
+    // returns a write stream to the file if it is empty
+    // overrides old content
+    if(this->isEmtpy()){
+        return this->getWriteStream();
+    } else{
+        PLOG_WARNING << "The file is not empty (file_path: " << this->filepath.c_str() << ")";
+        return ErrorStruct<std::ofstream>{SuccessType::FAIL, ErrorCode::ERR_FILE_NOT_EMPTY, this->filepath.c_str()};
+    }
+}
+
 std::filesystem::path FileHandler::getPath() const noexcept { return this->filepath; }
 
-ErrorStruct<bool> FileHandler::writeBytesIfEmpty(Bytes bytes) const noexcept {
+ErrorStruct<bool> FileHandler::writeBytesIfEmpty(Bytes& bytes) noexcept {
     // writes bytes to the file if the file is empty
     try {
         if (!this->isEmtpy()) {
@@ -196,34 +234,25 @@ Bytes FileHandler::getAllBytes() const {
     file.seekg(0, std::ios::beg);
 
     // gets the Bytes
-    std::vector<unsigned char> file_content(file_size);
-    file.read(reinterpret_cast<char*>(file_content.data()), file_size);
-    Bytes ret;
-    ret.setBytes(file_content);
+    Bytes ret(file_size);
+    file.read(reinterpret_cast<char*>(ret.getBytes()), file_size);
+    ret.setLen(file_size);
+    file.close();
     return ret;
 }
 
-Bytes FileHandler::getFirstBytes(const int num) const {
+Bytes FileHandler::getFirstBytes(const size_t num) const {
     // reads the num first bytes of the encryption file
-    std::ifstream file(this->filepath.c_str());  // create the file stream
+    std::ifstream file(this->filepath.c_str(), std::ios::binary);  // create the file stream
     if (!file) {
         // the set encryption file does not exist on the system
         throw std::runtime_error("file cannot be opened");
     }
-    // get length of file:
-    file.seekg(0, file.end);    // jump to the end
-    int length = file.tellg();  // saves the position
-    file.seekg(0, file.beg);    // jumps to the start again
-    if (length < num) {
+    Bytes b(num);        // creates a buffer to hold the read bytes
+    if(file.readsome(reinterpret_cast<char*>(b.getBytes()), num) != num);{  // reads into the buffer
         // not enough characters to read (file is not long enough)
         throw std::length_error("File contains to few characters");
     }
-    char buf[num];        // creates a buffer to hold the read bytes
-    file.read(buf, num);  // reads into the buffer
-    // transform the char array into an Bytes object
-    Bytes ret;
-    for (char c : buf) {
-        ret.addByte(static_cast<unsigned char>(c));  // cast from char to unsigned char
-    }
-    return ret;
+    file.close();
+    return b;
 }
